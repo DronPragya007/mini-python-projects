7df6d6aab5bacffb389fd6c986c54fc7e3034db8
DronPragya007 <pragyadron@gmail.com>
2025-11-30 03:29:23 +0530

    Create simple calulater with history.py

---
+++ simple calulater with history.py
@@ -0,0 +1,28 @@
+while True:
+    userinput=input("enter a equeation,history,exit,clear:".upper()).strip().lower()
+    if userinput == "history":
+        with open ('data.txt','r') as r:
+            his=r.readlines()
+            if his:
+
+                for i in his :
+                    print(i)
+            else:
+                print('no histry found ')
+    elif userinput == 'exit':
+        print('good bye!!!!!! SEE  YOU NEXT TIME !!(:')
+        break 
+    elif userinput=='clear':
+        with open ('data.txt','w') as c:
+            pass
+        print('history cleared!!!')
+    else:
+        try:
+            ans=eval(userinput)
+            print(ans)
+            with open("data.txt",'a') as f:
+                f.write(f'{userinput}={ans}')
+
+        except:
+            print('unetical input!!!')
+
